66193484a1c6dea4d78da2b8b30cb64b02b3fb4b
demandosigno <34424426+demandosigno@users.noreply.github.com>
2024-01-13 16:33:49 +0900

    ディクショナリ・タプル、セット

---
+++ chap02/2-20_tuple_type.py
@@ -0,0 +1,8 @@
+members = ['松田']
+members2 = {'name': '松田'}
+members3 = ('松田') # 要素数1のタプルを定義(したつもり)
+members4 = ('松田',)
+print(type(members))
+print(type(members2))
+print(type(members3))
+print(type(members4))
---
+++ chap02/2-22_set.py
@@ -0,0 +1,5 @@
+scores = {70, 80, 55, 80, 99, 200, 100}
+scores.add(80)
+print(scores)
+print(f'要素数は{len(scores)}')
+print(f'合計は{sum(scores)}')
